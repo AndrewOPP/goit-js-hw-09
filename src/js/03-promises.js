bb032e24366996c2d5ec57ecd24223ecb1d7ec4a
AndrewOPP <a.vyblov2003@gmail.com>
2023-08-18 22:55:10 +0300

    reworked

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,3 @@
-
 const buttonSubmit = document.querySelector("button")
 const form = document.querySelector("form")
 const { delay, step, amount } = form.elements
@@ -6,21 +5,33 @@ buttonSubmit.addEventListener("click", createPromise)
 let position = 0
 let stepValue = 0
 
+
+//Функция для решения задания
 function createPromise() {
+// Вызов первого элемента в нужное время
   setTimeout(() => {
-    for (let index = 0; index < amount.value; index += 1) {
+    newPromise(position, Number(delay.value))
+    stepValue += Number(delay.value)
+  }, delay.value)
+// Вызов остальных элементов в нужное время
+  setTimeout(() => {
+    for (let index = 0; index < amount.value - 1; index += 1) {
       position += 1
-      stepValue += Number(step.value)
+      stepValue +=  Number(step.value)
       newPromise(position, stepValue)
     }
   }, delay.value)
+// Обновление значений после конца действия 
   setTimeout(() => {
-    position = 0
+    position = 1
     stepValue = 0
   }, step.value * amount + delay.value)
+
 }
 
+  //Функция для создания промисов
 function newPromise(position, stepValue) {
+// Создание промисов
 let a = new Promise((resolve, reject) => {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -29,12 +40,14 @@ let a = new Promise((resolve, reject) => {
        reject()
     }
   }) 
+  // Обработка промисов
   setTimeout(() => {
     returnResult(a, position, stepValue)
-    stepValue = 0
-  }, stepValue)
+  }, stepValue - Number(delay.value))
 }
 
+
+  //Функция для обработкм промисов
 function returnResult(a, position, stepValue) {
   a.then(() => {
     console.log(`✅ Fulfilled promise ${position} in ${stepValue}ms`);
